c911be058af03b3598fd85d10d0fba34b10abbbb
NumeracyMuse <NumeracyMuse@gmail.com>
2015-02-22 16:09:17 -0700

    rdpend #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function to calculate the inverse of a matrix 
+## and then display the function
 
-## Write a short comment describing this function
-
+##   read in the matrix
 makeCacheMatrix <- function(x = matrix()) {
+        
+	##   hold the matrix
+        getnet <- function() x
+        holdnet <- function(write, getnet) 
+
+	##   display the the matrix
+        list(holdnet)
 
 }
 
-
-## Write a short comment describing this function
-
+ 
+## solve (inverse) the matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	    
+	##   invert the matrix
+        solvenet <- function(inverse, holdnet) 
+
+	##   display the the matrix
+        list(solvenet)
+	
+	
+	
 }
